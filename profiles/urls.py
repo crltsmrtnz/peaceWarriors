0a5df9dac3d1859ee6669838aa6ac73dfe090f5a
crlts <crltsmrtnz@gmail.com>
2019-06-27 17:28:53 -0400

    Se agrego login required a perfiles de usuarios

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 from .views import ProfileListView, ProfileDetailView
 
 profiles_patterns = ([
-    path('', ProfileListView.as_view(), name='list'),
-    path('<username>/', ProfileDetailView.as_view(), name='detail'),
+    path('', login_required(ProfileListView.as_view()), name='list'),
+    path('<username>/', login_required(ProfileDetailView.as_view()), name='detail'),
 ], "profiles")
